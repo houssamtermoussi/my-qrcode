5405fe0fa84ddb956f17634b8e738e44c26bffd4
houssamtermoussi <houssam111888@gmail.com>
2026-05-17 12:43:35 +0100

    cx a la db

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'qrcode_db',
+  database: process.env.DB_NAME || 'myqrcode_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
